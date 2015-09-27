87ccee6a188a1dac49e19599688d501703265d79
twillzy <william.tanzy@gmail.com>
2015-09-27 18:31:11 +1000

    user controller and model update, no more self loving and see only interested gener

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
 
   before_action :require_login
+  before_action :set_user, only: [:edit, :profile, :update, :destroy]
 
   def index
-      # if params[:id]
-      #   @users = User.gender(current_user).not_me(current_user).where('id < ?', params[:id]).limit(10) - current_user.matches(current_user)
-      # else
-      #   @users = User.gender(current_user).not_me(current_user).limit(10) - current_user.matches(current_user)
-      # end
-
       if params[:id]
-        @users = User.where('id < ?', params[:id]).limit(2)
+        @users = User.gender(current_user).not_me(current_user).where('id < ?', params[:id]).limit(10) - current_user.matches(current_user)
       else
-        @users = User.all.limit(2)
+        @users = User.gender(current_user).not_me(current_user).limit(10) - current_user.matches(current_user)
       end
+
+      # if params[:id]
+      #   @users = User.where('id < ?', params[:id]).limit(2)
+      # else
+      #   @users = User.all.limit(2)
+      # end
 
         respond_to do |format|
         format.html
@@ -25,11 +26,42 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(users_params)
+    respond_to do |format|
+      format.html {redirect_to users_path}
+    end
+    else
+      redirect_to edit_user_path(@user)
+    end
+
+  end
+
+  def destroy
+    if @user.destroy
+      session[:user_id] = nil
+      session[:omniauth] = nil
+      redirect_to root_path
+    else
+      redirect_to edit_user_path(@user)
+    end
+  end
+
   def profile
   end
 
   def matches
     @matches = current_user.friendships.where(state: "ACTIVE").map(&:friend) + current_user.inverse_friendships.where(state:"ACTIVE").map(&:user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def users_params
+    params.require(:user).permit(:interest, :bio, :avatar, :location, :date_of_birth)
   end
 end
 
