a4f80cb113da9b79a4e24f5946a2229ca1445fc3
twillzy <william.tanzy@gmail.com>
2015-09-27 13:35:42 +1000

    s3 credentials modifield

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module Tinderwar
         :s3_credentials => {
             :bucket => ENV['AWS_BUCKET'],
             :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+            :s3_host_name => "s3-ap-southeast-2.amazonaws.com"
         }
     }
 
