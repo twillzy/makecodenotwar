72584cb2622daea568c28abc4d1794315d96589c
Vivienne Lovelock <viv.lovelock@gmail.com>
2015-09-27 13:34:24 +1000

    jTinder fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,18 @@ class UsersController < ApplicationController
   before_action :require_login
 
   def index
-  	@users = User.all
+      if params[:id]
+        @users = User.gender(current_user).not_me(current_user).where('id < ?', params[:id]).limit(10) - current_user.matches(current_user)
+      else
+        @users = User.gender(current_user).not_me(current_user).limit(10) - current_user.matches(current_user)
+      end
+
+        respond_to do |format|
+        format.html
+        format.js
+      end
   end
+
 
   def edit
   end
