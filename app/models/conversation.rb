7dc0dc8b9b1e1c5aefe9977772bf34329429d9e8
twillzy <william.tanzy@gmail.com>
2015-09-29 14:25:56 +1000

    chat sorta done

---
+++ app/models/conversation.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id           :integer          not null, primary key
+#  sender_id    :integer
+#  recipient_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Conversation < ActiveRecord::Base
+	belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
+	belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
+
+	has_many :messages, dependent: :destroy
+
+	validates_uniqueness_of :sender_id, :scope => :recipient_id
+
+	# Scope that retrieves all conversations of the currently logged-in user
+	scope :involving, -> (user) do
+		where("conversations.sender_id =? OR conversations.recipient_id =?", user.id, user.id)
+	end
+
+	# Scope that checks if a conversation exists between any given two users before creating the conversation
+	scope :between, -> (sender_id, recipient_id) do
+		where("(conversations.sender_id =? AND conversations.recipient_id =?) OR (conversations.sender_id =? AND conversations.recipient_id=?)", sender_id,recipient_id, recipient_id, sender_id)
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ActiveRecord::Base
 
 	has_many :friendships, dependent: :destroy
 	has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+	has_many :conversations, :foreign_key => :sender_id
 
 	has_attached_file :avatar,
 					  :storage => :s3,
